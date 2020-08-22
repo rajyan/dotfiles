e7f24481751649dfca5a119f4c7c8eec2b986946
rajyan <kitakita7617@gmail.com>
2020-08-22 17:54:16 +0900

    added brew installation

--- init.sh
+++ init.sh
@@ -1,5 +1,11 @@
+#!/bin/bash
+
 for file in $(cat list)
 do
-    [ -f $HOME/$flie ] && mv -f $HOME/$file $HOME/$file.bak
+    [ -f $HOME/$file ] && mv -f $HOME/$file $HOME/$file.bak
     ln -s $HOME/dotfiles/$file $HOME/$file
 done
+
+eval $(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)
+eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+brew bundle
