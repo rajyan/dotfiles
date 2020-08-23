1fa015882faa463968c0983e76886465b6e00250
rajyan <kitakita7617@gmail.com>
2020-08-23 17:36:17 +0900

    fixed clean up

--- cleanup.sh
+++ cleanup.sh
@@ -1,8 +1,11 @@
 #! /bin/bash
 
+# clean up list
 for file in $(cat list)
 do
     [ ! -f $file ] && sed -i "/$file/d" list
 done
 
-ls -a $HOME | grep "^\..*\.bak$" | xargs rm -f
+# clean up .bak files
+ls -d -1 "$HOME"/.?*.bak 2> /dev/null | xargs rm -f
+rm -f Brewfile.bak
