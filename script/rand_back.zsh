2b1de810f8d83e235dc401b6fe8c8d6bd53fb680
yota-kimura <yota.kimura@gree.net>
2020-08-26 13:51:18 +0900

    changed to cp

--- script/rand_back.zsh
+++ script/rand_back.zsh
@@ -1,12 +1,9 @@
 #! /bin/zsh
 
 IMG_DIR=$HOME/Desktop/images
-NAME_BACKUP=$IMG_DIR/backup
-BACK_IMG=$IMG_DIR/back.png
+BACK_FILE=$IMG_DIR/back.png
 
-[[ -f $NAME_BACKUP ]] && mv -f $BACK_IMG $(cat $NAME_BACKUP)
-cnt=$( find $IMG_DIR -type f -regex '.*\..*' | wc -l )
-rand=$(( $RANDOM % $cnt + 1 ))
-rand_img=$( find $IMG_DIR -type f -regex '.*\..*' | sed -n ${rand}P )
-echo $rand_img >! $NAME_BACKUP
-mv -f $rand_img $BACK_IMG
+img_list=$( find $IMG_DIR -type f -regex '.*\..*' )
+rand=$(( $RANDOM % $( echo $img_list | wc -l ) + 1 ))
+rand_img=$( echo $img_list | sed -n ${rand}P )
+command cp $rand_img $BACK_FILE
