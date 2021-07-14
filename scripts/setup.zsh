985b620e31d8486b9ac08f320420ee5c1c64239c
yota-kimura <yota.kimura@gree.net>
2021-07-14 09:35:50 +0900

    fix copying same file

--- scripts/setup.zsh
+++ scripts/setup.zsh
@@ -1,9 +1,9 @@
 #! /bin/zsh
 
 IMG_DIR=$HOME/Desktop/images
-BACK_FILE=$IMG_DIR/back.png
+BACK_FILE=$IMG_DIR/back.raw
 
-img_list=$( find "$IMG_DIR" -type f -regex '.*\..*' )
+img_list=$( find -E "$IMG_DIR" -type f -regex '.*\.(png|jpg)' )
 rand=$(( RANDOM % $( echo "$img_list" | wc -l ) + 1 ))
 rand_img=$( echo "$img_list" | sed -n ${rand}P )
 command cp -f "$rand_img" "$BACK_FILE"
