740a956b541e466743c30d18aa4f10ffbc2731f9
Yohta Kimura <kitakita7617@gmail.com>
2021-08-06 03:38:17 +0900

    fix regex

--- scripts/setup.zsh
+++ scripts/setup.zsh
@@ -4,7 +4,7 @@ IMG_DIR=$HOME/Desktop/images
 BACK_FILE=$IMG_DIR/back.raw
 
 if [[ -d $IMG_DIR ]]; then
-    img_list=$( find -E "$IMG_DIR" -type f -regex '.*\.(png|jpg)' )
+    img_list=$( find "$IMG_DIR" -type f -regextype posix-extended -regex '.*\.(png|jpg)' )
     rand=$(( RANDOM % $( echo "$img_list" | wc -l ) + 1 ))
     rand_img=$( echo "$img_list" | sed -n ${rand}P )
     command cp -f "$rand_img" "$BACK_FILE"
