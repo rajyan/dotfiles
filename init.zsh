06d3fea2413a8cde8bd849fed4e242bba5fd9cbf
Yohta Kimura <kitakita7617@gmail.com>
2021-11-08 03:24:17 +0900

    add source han code jp font installation

--- init.zsh
+++ init.zsh
@@ -9,11 +9,17 @@ do
     ln -sv "$DOT_DIR/$file" "$HOME/$file"
 done < "$DOT_DIR/list"
 
+# run zinit
 zsh
 
 # install fonts
+## powerline fonts
 git clone https://github.com/powerline/fonts.git --depth=1 && fonts/install.sh && rm -rf fonts
 
-# gnome-terminal
-git clone https://github.com/arcticicestudio/nord-gnome-terminal.git && nord-gnome-terminal/src/nord.sh && rm -rf nord-gnome-terminal
+## source han code jp
+git clone https://github.com/adobe-type-tools/afdko.git
+git clone https://github.com/adobe-fonts/source-han-code-jp.git
+python -m venv afdko_env && source afdko_env/bin/activate && pip3 install afdko
+pushd source-han-code-jp && ./commands.sh && popd
+deactivate && rm -rf afdko* source-han-code-jp
 
