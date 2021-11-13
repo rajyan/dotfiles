b38c32e71ec97ea5106bdaa995593f1c9e3cfa3d
Yohta Kimura <kitakita7617@gmail.com>
2021-11-13 22:36:41 +0900

    fix installer

--- init.zsh
+++ init.zsh
@@ -12,9 +12,6 @@ do
     ln -svf "$filepath" "$HOME/$file"
 done
 
-# run zinit
-zsh
-
 # install fonts
 ## download powerline fonts
 git clone https://github.com/powerline/fonts.git --depth=1 && pushd fonts
@@ -22,7 +19,7 @@ git clone https://github.com/powerline/fonts.git --depth=1 && pushd fonts
 ## download and build source han code jp
 git clone https://github.com/adobe-fonts/source-han-code-jp.git
 git clone https://github.com/adobe-type-tools/afdko.git
-python -m venv afdko_env && source afdko_env/bin/activate && pip3 install afdko
+python -m venv afdko_env && source afdko_env/bin/activate && pip install afdko
 pushd source-han-code-jp && ./commands.sh && popd
 deactivate && rm -rf afdko* && popd
 
