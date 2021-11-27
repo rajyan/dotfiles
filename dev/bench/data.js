1d178e7b7b8c430cd3d7fb77a5cf636b9117cd48
github-action-benchmark <github@users.noreply.github.com>
2021-11-27 04:21:24 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for de3a52616e68e06b7429d67c995572af730b91d0

---
+++ dev/bench/data.js
@@ -0,0 +1,37 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1637986884569,
+  "repoUrl": "https://github.com/rajyan/dotfiles",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "kitakita7617@gmail.com",
+            "name": "Yohta Kimura",
+            "username": "rajyan"
+          },
+          "committer": {
+            "email": "kitakita7617@gmail.com",
+            "name": "Yohta Kimura",
+            "username": "rajyan"
+          },
+          "distinct": true,
+          "id": "de3a52616e68e06b7429d67c995572af730b91d0",
+          "message": "use atload if possible",
+          "timestamp": "2021-11-27T13:19:04+09:00",
+          "tree_id": "b06e925c5c92e73744525b594a2ebe0737f114c5",
+          "url": "https://github.com/rajyan/dotfiles/commit/de3a52616e68e06b7429d67c995572af730b91d0"
+        },
+        "date": 1637986883720,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "zsh load time Linux",
+            "value": "39.18398130231884",
+            "unit": "millisecond"
+          }
+        ]
+      }
+    ]
+  }
+}
