73443d106f0b509333d971c3b080a178f75f2bcf
Yohta Kimura <yota.kimura@gree.net>
2021-11-29 11:43:07 +0900

    explicitly use python3

--- fonts.zsh
+++ fonts.zsh
@@ -9,7 +9,7 @@ git clone https://github.com/powerline/fonts.git --depth=1 && pushd fonts
 ## download and build source han code jp
 git clone https://github.com/adobe-fonts/source-han-code-jp.git
 git clone https://github.com/adobe-type-tools/afdko.git
-python -m venv afdko_env && source afdko_env/bin/activate && pip install afdko
+python3 -m venv afdko_env && source afdko_env/bin/activate && pip3 install afdko
 pushd source-han-code-jp && ./commands.sh && popd
 deactivate && rm -rf afdko* && popd
 
