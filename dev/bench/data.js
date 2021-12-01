4e7eeeb4d49657f0175aa6a4d2f38668db38d7f7
rajyan <38206553+rajyan@users.noreply.github.com>
2021-12-02 08:43:17 +0900

    delete outliers

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1330,33 +1330,6 @@ window.BENCHMARK_DATA = {
           "tree_id": "c1798f3f791fcad3e3660f00a883606a4c990b82",
           "url": "https://github.com/rajyan/dotfiles/commit/6ac68ee62f9e0617125aff6cc10db65bc846af43"
         },
-        "date": 1638182195356,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "zsh load time Linux",
-            "value": "1444.8200435200001",
-            "unit": "millisecond"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
-            "email": "yota.kimura@gree.net",
-            "name": "Yohta Kimura"
-          },
-          "committer": {
-            "email": "yota.kimura@gree.net",
-            "name": "Yohta Kimura"
-          },
-          "distinct": true,
-          "id": "6ac68ee62f9e0617125aff6cc10db65bc846af43",
-          "message": "add brew bin",
-          "timestamp": "2021-11-29T19:01:51+09:00",
-          "tree_id": "c1798f3f791fcad3e3660f00a883606a4c990b82",
-          "url": "https://github.com/rajyan/dotfiles/commit/6ac68ee62f9e0617125aff6cc10db65bc846af43"
-        },
         "date": 1638183914505,
         "tool": "customSmallerIsBetter",
         "benches": [
@@ -1471,33 +1444,6 @@ window.BENCHMARK_DATA = {
           {
             "name": "zsh load time macOS",
             "value": "82.45174791",
-            "unit": "millisecond"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
-            "email": "yota.kimura@gree.net",
-            "name": "Yohta Kimura"
-          },
-          "committer": {
-            "email": "yota.kimura@gree.net",
-            "name": "Yohta Kimura"
-          },
-          "distinct": true,
-          "id": "6ac68ee62f9e0617125aff6cc10db65bc846af43",
-          "message": "add brew bin",
-          "timestamp": "2021-11-29T19:01:51+09:00",
-          "tree_id": "c1798f3f791fcad3e3660f00a883606a4c990b82",
-          "url": "https://github.com/rajyan/dotfiles/commit/6ac68ee62f9e0617125aff6cc10db65bc846af43"
-        },
-        "date": 1638192535072,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "zsh load time Linux",
-            "value": "794.8481742600001",
             "unit": "millisecond"
           }
         ]
