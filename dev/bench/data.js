02d1a139e64e3328dc548f570a4babe041706f86
github-action-benchmark <github@users.noreply.github.com>
2023-01-03 00:59:11 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for baa964bf74b3c8c2b27c861d89348b8a0393ed91

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1672707260288,
+  "lastUpdate": 1672707551946,
   "repoUrl": "https://github.com/rajyan/dotfiles",
   "entries": {
     "Benchmark": [
@@ -2561,6 +2561,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "zsh load time Linux",
             "value": "64.07218501581397",
+            "unit": "millisecond"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "38206553+rajyan@users.noreply.github.com",
+            "name": "Yohta Kimura",
+            "username": "rajyan"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "baa964bf74b3c8c2b27c861d89348b8a0393ed91",
+          "message": "Update .zshrc",
+          "timestamp": "2023-01-03T09:50:14+09:00",
+          "tree_id": "d8507a3b6cf0d144c1dcacfd1ddbc72024ca9986",
+          "url": "https://github.com/rajyan/dotfiles/commit/baa964bf74b3c8c2b27c861d89348b8a0393ed91"
+        },
+        "date": 1672707550393,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "zsh load time macOS",
+            "value": "93.79863259833331",
             "unit": "millisecond"
           }
         ]
