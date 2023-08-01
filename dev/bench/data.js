c5e551a5527a2cc002c9442fb9d97223751b75ce
github-action-benchmark <github@users.noreply.github.com>
2023-08-01 23:48:42 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 01f6af5dc21ee5d6a3a2287b79c377968a896c8e

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1690933232533,
+  "lastUpdate": 1690933722917,
   "repoUrl": "https://github.com/rajyan/dotfiles",
   "entries": {
     "Benchmark": [
@@ -3127,6 +3127,33 @@ window.BENCHMARK_DATA = {
           {
             "name": "zsh load time macOS",
             "value": "108.50919262727275",
+            "unit": "millisecond"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Yohta Kimura",
+            "username": "rajyan",
+            "email": "yota.kimura@gree.net"
+          },
+          "committer": {
+            "name": "Yohta Kimura",
+            "username": "rajyan",
+            "email": "yota.kimura@gree.net"
+          },
+          "id": "01f6af5dc21ee5d6a3a2287b79c377968a896c8e",
+          "message": "fix git-credential-manager org",
+          "timestamp": "2023-05-08T02:07:15Z",
+          "url": "https://github.com/rajyan/dotfiles/commit/01f6af5dc21ee5d6a3a2287b79c377968a896c8e"
+        },
+        "date": 1690933722037,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "zsh load time Linux",
+            "value": "76.89648072500002",
             "unit": "millisecond"
           }
         ]
