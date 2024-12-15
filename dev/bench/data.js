96a6f382ae3f6209b855f3cc1e473370b0aa9f9a
github-action-benchmark <github@users.noreply.github.com>
2024-12-15 08:46:16 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for ea63bbb08013ef9aab1e3d9f96e6743e86036a52

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1734252340251,
+  "lastUpdate": 1734252376869,
   "repoUrl": "https://github.com/rajyan/dotfiles",
   "entries": {
     "Benchmark": [
@@ -6545,6 +6545,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "zsh load time ubuntu-24.04",
             "value": "49.22571920999999",
+            "unit": "millisecond"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "38206553+rajyan@users.noreply.github.com",
+            "name": "Yohta Kimura",
+            "username": "rajyan"
+          },
+          "committer": {
+            "email": "38206553+rajyan@users.noreply.github.com",
+            "name": "Yohta Kimura",
+            "username": "rajyan"
+          },
+          "distinct": true,
+          "id": "ea63bbb08013ef9aab1e3d9f96e6743e86036a52",
+          "message": "fix: cleanup `$PATH` using zdharma-continuum/z-a-bin-gem-node",
+          "timestamp": "2024-12-15T17:42:37+09:00",
+          "tree_id": "3db4dc55fc088aaf5b4bdd53e00d30ff5e2fc16f",
+          "url": "https://github.com/rajyan/dotfiles/commit/ea63bbb08013ef9aab1e3d9f96e6743e86036a52"
+        },
+        "date": 1734252375869,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "zsh load time ubuntu-22.04",
+            "value": "52.85272748727273",
             "unit": "millisecond"
           }
         ]
