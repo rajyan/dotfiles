08c817d0292d1628d94b5a6d45f16e7a1703ef7f
github-action-benchmark <github@users.noreply.github.com>
2024-12-15 08:52:09 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for b80bd88317871445ae54214b0f34074b9dbba06e

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1734252376869,
+  "lastUpdate": 1734252729574,
   "repoUrl": "https://github.com/rajyan/dotfiles",
   "entries": {
     "Benchmark": [
@@ -6574,6 +6574,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "zsh load time ubuntu-22.04",
             "value": "52.85272748727273",
+            "unit": "millisecond"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "38206553+rajyan@users.noreply.github.com",
+            "name": "Yohta Kimura",
+            "username": "rajyan"
+          },
+          "committer": {
+            "email": "38206553+rajyan@users.noreply.github.com",
+            "name": "Yohta Kimura",
+            "username": "rajyan"
+          },
+          "distinct": true,
+          "id": "b80bd88317871445ae54214b0f34074b9dbba06e",
+          "message": "fix: fix hook error",
+          "timestamp": "2024-12-15T17:48:13+09:00",
+          "tree_id": "894a3a8f306325e94e299c68109ec0f734148975",
+          "url": "https://github.com/rajyan/dotfiles/commit/b80bd88317871445ae54214b0f34074b9dbba06e"
+        },
+        "date": 1734252728065,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "zsh load time macos-14",
+            "value": "43.47110634153845",
             "unit": "millisecond"
           }
         ]
