60a5056663ef0207668a9fccf61c17c036786809
github-action-benchmark <github@users.noreply.github.com>
2024-12-21 10:54:23 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 03a13f5ac942be40ea0d91504b96cd91040e5f77

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1734778440479,
+  "lastUpdate": 1734778463413,
   "repoUrl": "https://github.com/rajyan/dotfiles",
   "entries": {
     "Benchmark": [
@@ -7908,6 +7908,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "zsh load time macos-15",
             "value": "54.324509802264146",
+            "unit": "millisecond"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "38206553+rajyan@users.noreply.github.com",
+            "name": "Yohta Kimura",
+            "username": "rajyan"
+          },
+          "committer": {
+            "email": "38206553+rajyan@users.noreply.github.com",
+            "name": "Yohta Kimura",
+            "username": "rajyan"
+          },
+          "distinct": true,
+          "id": "03a13f5ac942be40ea0d91504b96cd91040e5f77",
+          "message": "fix: fix",
+          "timestamp": "2024-12-21T19:51:19+09:00",
+          "tree_id": "b3d2ab1c79874386201e4e0280c037b5c2277f19",
+          "url": "https://github.com/rajyan/dotfiles/commit/03a13f5ac942be40ea0d91504b96cd91040e5f77"
+        },
+        "date": 1734778463049,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "zsh load time ubuntu-22.04",
+            "value": "59.8089810275",
             "unit": "millisecond"
           }
         ]
