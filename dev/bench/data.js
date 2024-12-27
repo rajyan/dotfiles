f7e621e1ac55ccb5b5375219c12687351ef8cdc1
github-action-benchmark <github@users.noreply.github.com>
2024-12-27 07:47:53 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for d3b0fedacbdf43317a13f7ab306b019d8c78b198

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1735285652659,
+  "lastUpdate": 1735285673638,
   "repoUrl": "https://github.com/rajyan/dotfiles",
   "entries": {
     "Benchmark": [
@@ -8335,6 +8335,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "zsh load time macos-14",
             "value": "51.48873249387758",
+            "unit": "millisecond"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "38206553+rajyan@users.noreply.github.com",
+            "name": "Yohta Kimura",
+            "username": "rajyan"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "d3b0fedacbdf43317a13f7ab306b019d8c78b198",
+          "message": "Delete dots/.aliases",
+          "timestamp": "2024-12-27T16:44:55+09:00",
+          "tree_id": "a2f5f761dad079e770d463cedc8323eb1575931c",
+          "url": "https://github.com/rajyan/dotfiles/commit/d3b0fedacbdf43317a13f7ab306b019d8c78b198"
+        },
+        "date": 1735285672104,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "zsh load time macos-15",
+            "value": "72.87447641891893",
             "unit": "millisecond"
           }
         ]
