fb99ddc3aed7e1e3ff444c9fb3087756709f0849
github-action-benchmark <github@users.noreply.github.com>
2026-07-01 23:58:50 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 062e8c0b6a3f9b94edc45caa778e455a37c59838

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1782950328315,
+  "lastUpdate": 1782950330895,
   "repoUrl": "https://github.com/rajyan/dotfiles",
   "entries": {
     "Benchmark": [
@@ -10239,6 +10239,33 @@ window.BENCHMARK_DATA = {
           {
             "name": "zsh load time macos-15",
             "value": "73.92758852666665",
+            "unit": "millisecond"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Yohta Kimura",
+            "username": "rajyan",
+            "email": "38206553+rajyan@users.noreply.github.com"
+          },
+          "committer": {
+            "name": "Yohta Kimura",
+            "username": "rajyan",
+            "email": "38206553+rajyan@users.noreply.github.com"
+          },
+          "id": "062e8c0b6a3f9b94edc45caa778e455a37c59838",
+          "message": "fix: update wezterm source paths in .zshrc",
+          "timestamp": "2025-08-01T09:48:22Z",
+          "url": "https://github.com/rajyan/dotfiles/commit/062e8c0b6a3f9b94edc45caa778e455a37c59838"
+        },
+        "date": 1782950326004,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "zsh load time macos-14",
+            "value": "59.306870705833326",
             "unit": "millisecond"
           }
         ]
